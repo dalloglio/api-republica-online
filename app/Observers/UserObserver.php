97c66aafa0598e73a0199b23881a40c8fe2abffd
Ricardo Pires <ricardo.tech@live.com>
2017-06-12 22:36:31 -0300

    Não permitir excluir usuário com ID 1.

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Observers;
+
+use App\Domains\User;
+
+class UserObserver
+{
+    public function deleting(User $user)
+    {
+        if ($user->id == 1) {
+            throw new \Exception('O usuário principal não pode ser excluído.');
+        }
+    }
+}
