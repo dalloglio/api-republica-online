f7cd6f5eeeb123b8a96a88391aa125be1f374d5c
Ricardo Pires Dall Oglio <ricardo.tech@live.com>
2017-06-24 12:17:14 -0300

    Faker em português.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->singleton(\Faker\Generator::class, function () {
+                return \Faker\Factory::create(env('FAKER_LANGUAGE'));
+            });
         }
     }
 }
--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -4,7 +4,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\Domains\Address\Address::class, function (Faker\Generator $faker) {
     return [
-        'zip_code' => $faker->numerify('########'),
+        'zip_code' => str_replace(['-'], '', $faker->postcode),
         'street' => $faker->streetName,
         'number' => $faker->buildingNumber,
         'sub_address' => $faker->secondaryAddress,
