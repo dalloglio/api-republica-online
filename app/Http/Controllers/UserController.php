fa16d1ee2ee6a79db80ff944885647e8f5b90489
Ricardo Pires Dall Oglio <ricardo.tech@live.com>
2017-06-29 22:19:11 -0300

    Controllers utilizando trait CrudController.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,39 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Domains\User\UserRepository;
-use Illuminate\Http\Request;
+use App\Support\Traits\CrudController;
 
 class UserController extends Controller
 {
-    protected $user;
+    use CrudController;
+    
+    protected $repository;
 
-    public function __construct(UserRepository $user)
+    public function __construct(UserRepository $repository)
     {
-        $this->user = $user;
-    }
-
-    public function index()
-    {
-        return $this->user->getAll();
-    }
-
-    public function store(Request $request)
-    {
-        return $this->user->create($request->all());
-    }
-
-    public function show($id)
-    {
-        return $this->user->findById($id);
-    }
-
-    public function update(Request $request, $id)
-    {
-        return $this->user->edit($request->all(), $id);
-    }
-
-    public function destroy($id)
-    {
-        return $this->user->destroy($id);
+        $this->repository = $repository;
     }
 }
