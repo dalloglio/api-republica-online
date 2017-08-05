b8804a78043d093c0667f5abd5bbcec731574b94
Ricardo Pires <ricardo.tech@live.com>
2017-08-05 23:43:18 +0000

    Enviar e-mail de boas vindas quando um usuário for criado

--- app/Domains/User/Observers/UserObserver.php
+++ app/Domains/User/Observers/UserObserver.php
@@ -3,9 +3,15 @@
 namespace App\Domains\User\Observers;
 
 use App\Domains\User\User;
+use App\Mail\UserCreated;
 
 class UserObserver
 {
+    public function created(User $user)
+    {
+        Mail::to($user->email)->send(new UserCreated($user));
+    }
+
     public function deleting(User $user)
     {
         if ($user->id == 1) {
---
+++ resources/views/emails/users/created.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+# Seja bem vindo
+
+Olá {{ $user->name }}, a sua conta no {{ config('app.name') }} foi criada com sucesso e agora você já pode utilizar todos os recursos do site. Aproveite também para criar o seu primeiro anúncio.
+
+Seus dados de acesso:<br>
+E-mail: {{ $user->email }}<br>
+Senha: utilize a senha que você cadastrou.
+
+@component('mail::button', ['url' => $url, 'color' => 'blue'])
+Criar um anúncio
+@endcomponent
+
+Obrigado,<br>
+{{ config('app.name') }}
+@endcomponent
