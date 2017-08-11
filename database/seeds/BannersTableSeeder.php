a6d2f136cdedad2c2b7f70e84f88f1cd3cd7ba6b
Ricardo Pires <ricardo.tech@live.com>
2017-08-10 22:07:33 -0300

    Seed de banners com tamanhos corretos.

--- database/seeds/BannersTableSeeder.php
+++ database/seeds/BannersTableSeeder.php
@@ -11,10 +11,25 @@ class BannersTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create();
+
         factory(\App\Domains\Banner\Banner::class, 10)
             ->create()
-            ->each(function ($banner) {
-                $photo = factory(\App\Domains\Photo\Photo::class)->make();
+            ->each(function ($banner) use ($faker) {
+                $url = '';
+                if ($banner->size === '336x280') {
+                    $url = $faker->imageUrl(336, 280);
+                } else if ($banner->size === '300x600') {
+                    $url = $faker->imageUrl(300, 600);
+                } else if ($banner->size === '970x250') {
+                    $url = $faker->imageUrl(850, 250);
+                }
+                if ($url) {
+                    $photo = factory(\App\Domains\Photo\Photo::class)->make(['photo' => $url, 'url' => $url]);    
+                } else {
+                    $photo = factory(\App\Domains\Photo\Photo::class)->make();
+                }
+                $photo = factory(\App\Domains\Photo\Photo::class)->make(['photo' => $url, 'url' => $url]);
                 $banner->photo()->save($photo);
             });
     }
