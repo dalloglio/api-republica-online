135ede5ad78acf9c3c15133b11d33a9eb08d8075
Ricardo Pires Dall Oglio <ricardo.tech@live.com>
2017-10-18 00:38:22 -0200

    Retornar estados e cidades

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,7 @@ Route::get('/', function () {
     return redirect('/v1');
 });
 
-Route::get('teste', function() {
-
-    $guzzle = new GuzzleHttp\Client();
-
-    $response = $guzzle->post('http://localhost.api.republica.online/oauth/token', [
-        'form_params' => [
-            'grant_type' => 'client_credentials',
-            'client_id' => '2',
-            'client_secret' => 'lwrRZOG6GsN7ftYjbPLsulcUfC9v9eGNI5ZDXrLL',
-            'scope' => '',
-        ],
-    ]);
-    $token = (string) $response->getBody();
-    dd(json_decode($token));
-
-});
-
 Route::get('photo/{id}', 'PhotoController@photo')->middleware('api');
+
+Route::get('estados.json', 'JsonController@estados')->middleware('api');
+Route::get('cidades.json', 'JsonController@cidades')->middleware('api');
