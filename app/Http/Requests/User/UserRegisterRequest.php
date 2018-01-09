11209b4d8cc58621757ba05d69637e5f058138e5
Ricardo Pires Dall Oglio <ricardo.tech@live.com>
2018-01-09 08:42:04 -0200

    Validar usuário

--- app/Http/Requests/User/UserRegisterRequest.php
+++ app/Http/Requests/User/UserRegisterRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\User;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserRequest extends FormRequest
+class UserRegisterRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class UserRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/User/UserStoreRequest.php
+++ app/Http/Requests/User/UserStoreRequest.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Requests\Ad;
+namespace App\Http\Requests\User;
 
+use App\Domains\User\User;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class AdStoreRequest extends FormRequest
+class UserStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,24 +26,27 @@ class AdStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|max:255',
-            'description' => 'required|max:500',
-            'price' => 'required',
-            'category_id' => 'required|exists:categories,id',
-            'user_id' => 'required|exists:users,id',
+            'name' => 'required|max:255',
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'birthday' => 'required|date|date_format:Y-m-d|max:255',
+            'email' => 'required|email|unique:users,email|max:255',
+            'password' => 'required|min:6|max:20',
+            'gender' => [
+                'required',
+                Rule::in(User::genders())
+            ],
+            'cpf' => 'required|cpf|unique:users,cpf',
+            'status' => 'required|in:1,0',
 
             'address.zip_code' => 'required|max:9',
             'address.street' => 'required|max:255',
             'address.number' => 'required|max:20',
-            'address.sub_address' => 'max:255',
+            'address.sub_address' => 'sometimes|nullable|max:255',
             'address.neighborhood' => 'required|max:255',
             'address.state' => 'required|max:255',
             'address.state_initials' => 'required|max:2',
             'address.city' => 'required|max:255',
-
-            'contact.name' => 'required|max:255',
-            'contact.cellphone' => 'required|celular|max:15',
-            'contact.whatsapp' => 'required|celular|max:15',
         ];
     }
 }
